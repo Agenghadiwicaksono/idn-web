65f52aabb86bc91e68e0c5c285694987c761625b
agenghw27@gmail.coml <agenghadiwicaksono@MacBook-Pro-Ageng.local>
2023-11-29 20:15:12 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// toggle icon navbar/ humberger button
+let menuIcon =document.querySelector('#menu-icon');
+let navbar =document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
